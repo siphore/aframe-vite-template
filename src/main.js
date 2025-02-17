7165763266311e7ba299320caa02080a333d327b
ali baba <michael@cheneval.com>
2025-02-17 16:15:14 +0100

    Tests

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-import AFRAME from 'aframe';
-import './components/random-color.js';
+import AFRAME from "aframe";
+import "./components/random-color.js";
+
+AFRAME.registerComponent("modify-materials", {
+  init: function () {
+    // Wait for model to load.
+    this.el.addEventListener("model-loaded", () => {
+      // Grab the mesh / scene.
+      const obj = this.el.getObject3D("mesh");
+      // Go over the submeshes and modify materials we want.
+      obj.traverse((node) => {
+        if (node.name.indexOf("ship") !== -1) {
+          node.material.color.set("red");
+        }
+      });
+    });
+  },
+});
