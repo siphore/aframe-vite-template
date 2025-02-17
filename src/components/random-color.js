1d42f3247d1f9d2c3a56bbbd74024b697cedd5b8
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-02-17 10:15:22 +0100

    change random color to HSL format

--- src/components/random-color.js
+++ src/components/random-color.js
@@ -1,64 +1,48 @@
 AFRAME.registerComponent("random-color", {
 
-    schema:
-    {
-        // Time in milliseconds (ms).
-        minimumInterval: { type: "number", default: 2000 }
-    },
+  schema: {
+    // Time in milliseconds (ms).
+    minimumInterval: { type: "number", default: 2000 }
+  },
 
-    init: function()
-    {
-        this.throttleTick();
-    },
+  init: function () {
+    this.throttleTick();
+  },
 
-    update: function()
-    {
-        
-    },
+  update: function () {
 
-    tick: function()
-    {
-        this.el.setAttribute(
-            "material",
-            "color",
-            `${ this.getRandomHexColor() }`
-        );
-    },
+  },
 
-    remove: function()
-    {
-        
-    },
+  tick: function () {
+    this.el.setAttribute(
+      "material",
+      "color",
+      `${this.getRandomHslColor()}`
+    );
+  },
 
-    pause: function()
-    {
-        
-    },
+  remove: function () {
 
-    play: function()
-    {
-        
-    },
+  },
 
-    throttleTick: function()
-    {
-        this.tick = AFRAME.utils.throttleTick(
-            this.tick,
-            this.data.minimumInterval,
-            this
-        );
-    },
+  pause: function () {
 
-    getRandomHexColor: function()
-    {
-        const letters = "0123456789ABCDEF";
-        let color = "#";
+  },
 
-        for ( let i = 0; i < 6; i++) {
-            color += letters[ Math.floor( Math.random() * 16 ) ];
-        }
+  play: function () {
 
-        return color;
-    }
+  },
+
+  throttleTick: function () {
+    this.tick = AFRAME.utils.throttleTick(
+      this.tick,
+      this.data.minimumInterval,
+      this
+    );
+  },
+
+  getRandomHslColor: function () {
+    return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+  }
 
 });
